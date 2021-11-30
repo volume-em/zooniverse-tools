a178d21da4c83264fcb275a52b7f0b4ca2f967b4
conradry <conradrw@nih.gov>
2021-11-30 11:55:00 -0500

    added upload notebok

--- prep_stacks.py
+++ prep_stacks.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from glob import glob
 from skimage import io
 from skimage import transform
-from skimage.exposure import equalize_hist
+from skimage.exposure import equalize_hist, rescale_intensity
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -36,16 +36,11 @@ if __name__ == '__main__':
         for i, image in enumerate(stack):
             # resize the image
             image = transform.resize(image, (size, size), preserve_range=True)
+            image = np.clip(image, 0, 255).astype(np.uint8)
             
             # fix the contrast
             if contrast:
-                image = equalize_hist(image)
-                image -= image.min()
-                image /= image.max()
-                image *= 255
-                image = image.astype(np.uint8)
-            else:
-                image = np.clip(image, 0, 255).astype(np.uint8)
+                image = rescale_intensity(image, in_range=(0, 255), out_range=(25, 230))
                 
             # save the jpg
             out_fname = os.path.join(savedir, fname.replace('.tif', f'_{i}.jpg'))
