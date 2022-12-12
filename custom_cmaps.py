6fa62dd13bea85215a2320d58277505ea9735b7a
conradry <conradrw@nih.gov>
2022-12-12 11:13:40 -0500

    note about colormaps

--- custom_cmaps.py
+++ custom_cmaps.py
@@ -1,21 +1,11 @@
+"""
+Custom colormap for pyplot used to display cleaner segmentation results
+with a transparent background.
+"""
+
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-
-# Create a custom colormap so that background is transparent
-# get colormap
-ncolors = 256
-color_array = plt.get_cmap('plasma')(range(ncolors))
-
-# change alpha values
-#color_array[:,-1] = np.linspace(0.0, 1.0, ncolors)
-color_array[0, -1] = 0.0
-
-# create a colormap object
-map_object = LinearSegmentedColormap.from_list(name='plasma_alpha', colors=color_array)
-
-# register this new colormap with matplotlib
-plt.register_cmap(cmap=map_object)
 
 # Create a custom colormap so that background is transparent
 # get colormap
@@ -24,7 +14,6 @@ color_array = plt.get_cmap('hsv')(range(ncolors))
 color_array = np.random.permutation(color_array)
 
 # change alpha values
-#color_array[:,-1] = np.linspace(0.0, 1.0, ncolors)
 color_array[0, -1] = 0.0
 
 # create a colormap object
